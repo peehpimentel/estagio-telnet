9873475cbf7110ea941c98e4c6dd3967ec1632e3
Pedro Henrique Mello <peehpimentel@hotmail.com>
2024-12-20 08:56:59 -0400

    Fixed an error where a part of the work service route was unreachable.

--- src/app/api/os/route.ts
+++ src/app/api/os/route.ts
@@ -13,25 +13,29 @@ export async function POST(req: Request) {
 
     const existingRecord = await prisma.su_oss_chamado.findFirst({
       where: {
-        id_ticket: body.id
+        id_ticket: id,
       },
       select: {
         id: true,
-      }
+      },
     });
 
-if (existingRecord) {
-  return NextResponse.json({
-    data: existingRecord,
-  });
-}
-console.log('prisma:', existingRecord.id);
+    if (!existingRecord) {
+      console.error('No existing record found.');
+      return NextResponse.json(
+        { error: 'No existing record found for the given ID.' },
+        { status: 404 }
+      );
+    }
+
+    console.log('prisma:', existingRecord.id);
+
     const param = {
-      id_chamado: existingRecord, // Renomeado conforme necessário
+      id_chamado: existingRecord.id, // Renomeado conforme necessário
       mensagem: menssagem,
       data_agendamento: data_reservada,
       status: 'AG',
-      id_tecnico: '1'
+      id_tecnico: '1',
     };
 
     const url = `${process.env.SECRET_API}/su_oss_chamado_reagendar`!;
@@ -41,7 +45,7 @@ console.log('prisma:', existingRecord.id);
 
     const raw = JSON.stringify({
       ...param,
-      redirect: "follow",
+      redirect: 'follow',
     });
 
     const response = await fetch(url, {
@@ -59,6 +63,7 @@ console.log('prisma:', existingRecord.id);
     return NextResponse.json({
       success: true,
       response: data,
+      param: param, // Inclui o objeto param na resposta final
     });
   } catch (error: any) {
     console.error('Error in Rota 2:', error);
